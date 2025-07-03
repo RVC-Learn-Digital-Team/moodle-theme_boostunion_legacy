0dfb4c055d4de04ca2f639b8a014f2625a217d93
Brian Merritt <brianlmerritt@2111co.com>
2025-07-03 14:49:42 +0100

    Update version.php
    We are now approaching release candidate

--- version.php
+++ version.php
@@ -4,9 +4,9 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component   = 'theme_boostunion_legacy';
-$plugin->version     = 2025053012;   // YYYYMMDDXX – bump on each release
+$plugin->version     = 2025070201;
 $plugin->release     = 'v1.0.0';
-$plugin->maturity    = MATURITY_ALPHA;
+$plugin->maturity    = MATURITY_RC;
 
 // Must have Boost Union already installed (change version to match yours)
 $plugin->dependencies = [
